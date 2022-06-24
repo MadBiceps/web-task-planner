314f69d214f076ef1cb4a3917d038f3bb6b6960a
Fabian Fischer <fabian.niklas.fischer@gmail.com>
2022-06-24 09:24:48 +0200

    Create database models

---
+++ src/backend/WebAPI/WebAPI/Models/Database/Task.cs
@@ -0,0 +1,15 @@
+﻿using WebAPI.Models.Internal;
+
+namespace WebAPI.Models.Database;
+
+public class Task
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime PlannedEndDate { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public EStatus Status { get; set; }
+    public Category? Category { get; set; }
+}
