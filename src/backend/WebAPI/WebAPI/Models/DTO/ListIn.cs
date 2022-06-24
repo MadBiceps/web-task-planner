503e71745f8878c1574367ca01452eb13e87dcfc
Fabian Fischer <fabian.niklas.fischer@gmail.com>
2022-06-24 09:25:30 +0200

    Create DTO models

---
+++ src/backend/WebAPI/WebAPI/Models/DTO/ListIn.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebAPI.Models.Database;
+
+namespace WebAPI.Models.DTO;
+
+public class ListIn
+{
+    [MaxLength(500)]
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public List GetMapped()
+    {
+        return new List
+        {
+            Id = Guid.NewGuid(),
+            Name = Name,
+            Description = Description,
+        };
+    }
+    
+    public List GetMapped(Guid id)
+    {
+        var retVal = GetMapped();
+        retVal.Id = id;
+        return retVal;
+    }
+}
---
+++ src/backend/WebAPI/WebAPI/Models/DTO/TaskIn.cs
@@ -0,0 +1,36 @@
+﻿using WebAPI.Models.Internal;
+using Task = WebAPI.Models.Database.Task;
+
+namespace WebAPI.Models.DTO;
+
+public class TaskIn
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime PlannedEndDate { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public EStatus Status { get; set; }
+    
+    
+    public Task GetMapped()
+    {
+        return new Task
+        {
+            Id = Guid.NewGuid(),
+            Name = Name,
+            Description = Description,
+            PlannedEndDate = PlannedEndDate,
+            Status = Status,
+            StartDate = StartDate,
+            EndDate = EndDate
+        };
+    }
+    
+    public Task GetMapped(Guid id)
+    {
+        var retVal = GetMapped();
+        retVal.Id = id;
+        return retVal;
+    }
+}
