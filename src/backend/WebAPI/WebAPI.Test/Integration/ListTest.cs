fdcec12bf5d7a9c2ee4228723d0dd3a7f2486287
Fabian Fischer <fabian.niklas.fischer@gmail.com>
2022-06-27 06:53:31 +0200

    Implement integration tests

---
+++ src/backend/WebAPI/WebAPI.Test/Integration/ListTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using WebAPI.Models.Database;
+using WebAPI.Models.Internal;
+using List = WebAPI.Models.Database.List;
+
+namespace WebAPI.Test.Integration;
+
+public class ListTest : IntegrationTest
+{
+    private readonly List<List> Lists = new();
+
+    public ListTest()
+    {
+        for (var i = 0; i <= 10; i++)
+        {
+            Lists.Add(new List
+            {
+                Id = Guid.NewGuid(),
+                Name = $"List No. {i}",
+                Description = $"Description of list No. {i}",
+                Tasks = new List<Task>()
+            });
+        }
+
+        foreach (var list in Lists)
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                list.Tasks.Add(new Task
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Task No. {i}",
+                    Description = $"Description of task No. {i}",
+                    PlannedEndDate = DateTime.Now,
+                    Status = EStatus.NOT_STARTED
+                });
+            }
+        }
+    }
+
+    [SetUp]
+    public async System.Threading.Tasks.Task Setup()
+    {
+        await Database.AddRangeAsync(Lists);
+        await Database.SaveChangesAsync();
+    }
+
+    [TearDown]
+    public async System.Threading.Tasks.Task TearDown()
+    {
+        Database.RemoveRange(await Database.Lists.ToListAsync());
+        await Database.SaveChangesAsync();
+    }
+    
+    
+
+}
---
+++ src/backend/WebAPI/WebAPI.Test/IntegrationTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using WebAPI.Infrastructure;
+
+namespace WebAPI.Test;
+
+public class IntegrationTest
+{
+    protected readonly HttpClient ApiClient;
+    protected readonly DatabaseContext Database;
+    
+    protected IntegrationTest()
+    {
+        var appFactory = new WebApplicationFactory<Startup>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // Name has to be unique per instance because if not tests cant run in parallel
+                    var databaseName = Guid.NewGuid().ToString();
+                    // Setup database
+                    services.RemoveAll(typeof(DatabaseContext));
+                    services.RemoveAll(typeof(IDbContextFactory<DatabaseContext>));
+                    services.RemoveAll(typeof(DbContextOptions<DatabaseContext>));
+                    services.AddDbContext<DatabaseContext>(opt => opt
+                        .UseInMemoryDatabase(databaseName)
+                        .EnableDetailedErrors()
+                        .EnableSensitiveDataLogging(), ServiceLifetime.Transient);
+                    services.AddDbContextFactory<DatabaseContext>(opt => opt
+                        .UseInMemoryDatabase(databaseName)
+                        .EnableDetailedErrors()
+                        .EnableSensitiveDataLogging(), ServiceLifetime.Transient);
+                });
+            });
+
+        ApiClient = appFactory.CreateClient();
+        var serviceScope = appFactory.Services.CreateScope();
+        Database = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    }
+}
